f9d8433e5f9ac829f6405d4e1b7b765d34d8bd23
appitsolution <applicationitsolution@gmail.com>
2023-06-20 19:21:29 +0300

    test dhl

--- controllers/orders/addOrder.js
+++ controllers/orders/addOrder.js
@@ -4,6 +4,15 @@ const SchemeUser = require("../../db/SchemaUser");
 // import payload from "payload";
 
 const addOrder = async (req, res) => {
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + 7);
+
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+
+  const formattedDate = `${year}-${month}-${day}`;
+
   const { idUser, payment, products } = req.body;
 
   const user = await SchemeUser.findById(idUser);
@@ -36,9 +45,73 @@ const addOrder = async (req, res) => {
     data: data,
   });
 
+  const configDeliveryDHL = {
+    method: "post",
+    url: "https://api-sandbox.dhl.com/parcel/de/shipping/v1/orders?=false",
+    headers: {
+      Accept: "application/json",
+      "dhl-api-key": "FGAJK85YWwJuOoDdzvewskAGN0velKyG",
+      "Content-Type": "application/json",
+      Authorization:
+        "Basic MjIyMjIyMjIyMl9hYnJfMDgwMTpTOFBqbUxCIXMydnJ6V1Yzbw==",
+      "Access-Control-Allow-Origin": "*",
+    },
+    data: {
+      customerDetails: {
+        billingNumber: String(new Date()),
+      },
+      pickupLocation: {
+        pickupAddress: {
+          name1: "Deutsche Post DHL Group",
+          addressStreet: user.address.house,
+          addressHouse: user.address.house,
+          postalCode: user.address.index,
+          city: user.address.city,
+          country: "DE",
+        },
+      },
+      contactPerson: {
+        name: `${user.firstName} ${user.lastName}`,
+        phone: user.tel,
+        email: user.email,
+      },
+      pickupDetails: {
+        pickupDate: {
+          type: "Date",
+          value: formattedDate,
+        },
+        emailNotification: user.email,
+      },
+      shipmentDetails: {
+        shipments: [
+          {
+            transportationType: "PAKET",
+            pickupServices: {
+              bulkyGood: {
+                comment: "Fahrrad",
+              },
+            },
+            customerReference: "987654321",
+          },
+        ],
+      },
+    },
+  };
+
+  const createDeliveryDhl = await axios.request(configDeliveryDHL);
+
+  await payload.update({
+    collection: "orders-user",
+    id: result.id,
+    data: {
+      numberInvoice: createDeliveryDhl.data.confirmation.value.orderID,
+    },
+  });
+
   console.log(result);
   await SchemeUser.findByIdAndUpdate(idUser, {
     mySell: [...user.mySell, { id: result.id }],
+    numberInvoice: createDeliveryDhl.data.confirmation.value.orderID,
   });
 
   return res.send(result);
